613cea0e6fe810b09de158ec65fe65598d6c8c46
Levon Karakhanyan <levon98@bk.ru>
2021-12-17 16:20:20 +0300

    added animation to buttons

--- MathsQuiz/App/SceneDelegate.swift
+++ MathsQuiz/App/SceneDelegate.swift
@@ -21,6 +21,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         guard let windowScene = (scene as? UIWindowScene) else { return }
         
         window = UIWindow(frame:  UIScreen.main.bounds)
+//        window?.rootViewController = UserAccountViewController()
         window?.rootViewController = UINavigationController()
         
         appCoordinator.start()
