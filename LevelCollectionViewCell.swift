e424286eda28ab1bf50ea0f4f1876179db212fc6
Tigran Karakhanyan <lev.98@bk.ru>
2021-12-20 11:01:09 +0300

    added blocked cell form

--- LevelCollectionViewCell.swift
+++ LevelCollectionViewCell.swift
@@ -12,40 +12,37 @@ class LevelCollectionViewCell: UICollectionViewCell, ConfigCell {
     
     static var reuseId: String = "UICollectionViewCell"
     
+    var isCellBlocked = false
+    
     private let levelNumLabel = UILabel()
     private var starImageView: [UIImageView] = []
+    private let lockImageView = UIImageView()
+    
     
     func configure(with value: ActivityType) {
         self.backgroundColor = value.color
         self.layer.cornerRadius = 20
-        setupLevelNumLabel()
-        setupStarImageView()
+        if isCellBlocked {
+            setupBlockedCellForm()
+        } else {
+            setupUnblockedCellForm()
+        }
     }
     
-    func setupLevelNumLabel() {
+    func setupUnblockedCellForm() {
         levelNumLabel.text = "1"
         levelNumLabel.font = MQFont.systemFont48
         levelNumLabel.textAlignment = .center
         levelNumLabel.adjustsFontSizeToFitWidth = true
         levelNumLabel.translatesAutoresizingMaskIntoConstraints = false
         
-        self.addSubview(levelNumLabel)
-        
-        NSLayoutConstraint.activate([
-            levelNumLabel.centerXAnchor.constraint(equalTo: self.centerXAnchor),
-            levelNumLabel.topAnchor.constraint(equalTo: self.topAnchor, constant: 20)
-        ])
-    }
-    
-    func setupStarImageView() {
         for _ in 0...2 {
             let image = UIImageView()
             image.image = UIImage(systemName: "star.fill")
             image.contentMode = .scaleAspectFit
-            image.tintColor = MQColor.lavenderDark
+            image.tintColor = MQColor.background
             image.transform = .init(rotationAngle: 0.26)
             image.heightAnchor.constraint(equalToConstant: 26).isActive = true
-            image.widthAnchor.constraint(equalToConstant: 26).isActive = true
             starImageView.append(image)
         }
         
@@ -55,13 +52,33 @@ class LevelCollectionViewCell: UICollectionViewCell, ConfigCell {
         stackView.spacing = 1
         stackView.translatesAutoresizingMaskIntoConstraints = false
         
+        self.addSubview(levelNumLabel)
         self.addSubview(stackView)
         
         NSLayoutConstraint.activate([
+            levelNumLabel.centerXAnchor.constraint(equalTo: self.centerXAnchor),
+            levelNumLabel.topAnchor.constraint(equalTo: self.topAnchor, constant: 20),
+        
             stackView.topAnchor.constraint(equalTo: levelNumLabel.bottomAnchor, constant: 2),
             stackView.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 5),
             stackView.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -5),
             stackView.bottomAnchor.constraint(equalTo: self.bottomAnchor, constant: -5)
+        ])
+    }
+    
+    func setupBlockedCellForm() {
+        lockImageView.image = UIImage(systemName: "lock.fill")
+        lockImageView.tintColor = MQColor.lavenderDark
+        lockImageView.contentMode = .scaleAspectFit
+        lockImageView.translatesAutoresizingMaskIntoConstraints = false
+        
+        self.addSubview(lockImageView)
+        
+        NSLayoutConstraint.activate([
+            lockImageView.topAnchor.constraint(equalTo: self.topAnchor, constant: 12),
+            lockImageView.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 12),
+            lockImageView.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -12),
+            lockImageView.bottomAnchor.constraint(equalTo: self.bottomAnchor, constant: -12)
         ])
     }
 }
