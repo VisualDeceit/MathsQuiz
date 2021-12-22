28c194bee6e6d1e4266e13f9c639269f132d978f
Tigran Karakhanyan <lev.98@bk.ru>
2021-12-22 12:43:08 +0300

    added ChangePasswordViewController

--- MathsQuiz/App/SceneDelegate.swift
+++ MathsQuiz/App/SceneDelegate.swift
@@ -20,7 +20,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         guard let windowScene = (scene as? UIWindowScene) else { return }
         
         window = UIWindow(frame: UIScreen.main.bounds)
-         window?.rootViewController = UserDataViewController()
+         window?.rootViewController = ChangePasswordViewController()
 //        window?.rootViewController = UINavigationController()
         window?.windowScene = windowScene
         window?.makeKeyAndVisible()
