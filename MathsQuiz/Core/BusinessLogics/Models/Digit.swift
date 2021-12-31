614f9d796336f460ad9f2bedb2cfd551d0abcd40
Alexander Fomin <inzhener@outlook.com>
2021-12-31 09:51:41 +0300

    added Digit model

---
+++ MathsQuiz/Core/BusinessLogics/Models/Digit.swift
@@ -0,0 +1,13 @@
+//
+//  Digit.swift
+//  MathsQuiz
+//
+//  Created by Александр Фомин on 30.12.2021.
+//
+
+import Foundation
+
+struct Digit: Equatable, Hashable {
+    var value: Int
+    var carry: Int
+}
