af2f1a7fade6bce7813cf019df3d948283f1f9bd
Alexander Fomin <inzhener@outlook.com>
2022-01-07 11:23:43 +0300

    Added example view builder

---
+++ MathsQuiz/Core/BusinessLogics/Builder/ExampleViewBuilder.swift
@@ -0,0 +1,120 @@
+//
+//  ExampleViewBuilder.swift
+//  MathsQuiz
+//
+//  Created by Александр Фомин on 07.01.2022.
+//
+
+import UIKit
+
+class ExampleViewBuilder {
+    
+    private var horizontalStackView: UIStackView?
+    private var verticalStackView: UIStackView
+    
+    init() {
+        verticalStackView = UIStackView()
+        verticalStackView.translatesAutoresizingMaskIntoConstraints = false
+        verticalStackView.spacing = 4
+        verticalStackView.axis = .vertical
+        verticalStackView.alignment = .trailing
+        verticalStackView.distribution = .equalSpacing
+    }
+    
+    private func makeView() -> UIStackView {
+        let sv = UIStackView()
+        sv.translatesAutoresizingMaskIntoConstraints = false
+        sv.spacing = 4
+        sv.axis = .horizontal
+        sv.alignment = .bottom
+        sv.distribution = .equalSpacing
+        return sv
+    }
+    
+    private func addStackViewAndNil() {
+        if let horizontalStackView = horizontalStackView {
+            verticalStackView.addArrangedSubview(horizontalStackView)
+        }
+        self.horizontalStackView = nil
+    }
+    
+    func addNewRow() {
+        if let horizontalStackView = horizontalStackView {
+            verticalStackView.addArrangedSubview(horizontalStackView)
+            self.horizontalStackView = makeView()
+        } else {
+            self.horizontalStackView = makeView()
+        }
+    }
+    
+    func addDigit(_ digit: Digit, type: DigitType, index: Int) {
+        let view = UIView()
+        let digitView = ExampleDigitView(digit: String(digit.value), type: type, index: index)
+        let carryView = ExampleCarryView(frame: .zero, carry: "0")
+        
+        view.addSubview(digitView)
+        
+        horizontalStackView?.addArrangedSubview(view)
+        
+        if digit.carry != 0 {
+            let carryFrameSize: CGFloat = 25
+            
+            view.addSubview(carryView)
+            
+            NSLayoutConstraint.activate([
+                carryView.widthAnchor.constraint(equalToConstant: carryFrameSize),
+                carryView.heightAnchor.constraint(equalToConstant: carryFrameSize),
+                carryView.centerXAnchor.constraint(equalTo: view.trailingAnchor, constant: -carryFrameSize / 2),
+                carryView.topAnchor.constraint(equalTo: view.topAnchor),
+                
+                digitView.widthAnchor.constraint(equalToConstant: 50),
+                digitView.heightAnchor.constraint(equalToConstant: 70),
+                digitView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+                digitView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+                
+                view.widthAnchor.constraint(equalToConstant: 50),
+                view.heightAnchor.constraint(equalToConstant: 70 + carryFrameSize),
+            ])
+        } else {
+            NSLayoutConstraint.activate([
+                digitView.widthAnchor.constraint(equalToConstant: 50),
+                digitView.heightAnchor.constraint(equalToConstant: 70),
+                digitView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+                digitView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+                
+                view.widthAnchor.constraint(equalToConstant: 50),
+                view.heightAnchor.constraint(equalToConstant: 70),
+            ])
+        }
+    }
+
+    
+    func addSign(_ sign: ActivityType) {
+        let digitView = ExampleDigitView(sign: sign)
+        horizontalStackView?.addArrangedSubview(digitView)
+        
+        NSLayoutConstraint.activate([
+            digitView.widthAnchor.constraint(equalToConstant: 50),
+            digitView.heightAnchor.constraint(equalToConstant: 70)
+        ])
+    }
+    
+    func addSeparator(for digits: Int) {
+        addStackViewAndNil()
+        
+        let separator = UIView()
+        self.verticalStackView.addArrangedSubview(separator)
+        separator.backgroundColor = .systemGray4
+ 
+        NSLayoutConstraint.activate([
+            separator.widthAnchor.constraint(equalToConstant: CGFloat(54 * digits)),
+            separator.heightAnchor.constraint(equalToConstant: 4)
+        ])
+    }
+    
+    func build() -> UIStackView {
+        addStackViewAndNil()
+        return verticalStackView
+    }
+}
+
---
+++ MathsQuiz/Flows/Main/Example/Views/ExampleDigitView.swift
@@ -0,0 +1,117 @@
+//
+//  ExampleDigitView.swift
+//  MathsQuiz
+//
+//  Created by Александр Фомин on 07.01.2022.
+//
+
+import UIKit
+
+enum DigitType {
+    case common, result
+}
+
+class ExampleDigitView: UIView {
+    
+    let digit: String
+    let type: DigitType
+    let index: Int
+    
+    private let rectBorderWidth: CGFloat = 2
+    private let rectCornerRadius: CGFloat = 6
+    
+    private var digitColor: UIColor = .systemGray4
+    private var borderColor: UIColor = .systemGray4
+    
+    private(set) lazy var digitLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.font = UIFont.systemFont(ofSize: 80, weight: .regular)
+        label.textColor = digitColor
+        label.text = digit
+        return label
+    }()
+    
+    init(frame: CGRect, digit: String, type: DigitType, index: Int) {
+        self.digit = digit
+        self.type = type
+        self.index = index
+        super.init(frame: frame)
+        setupView()
+    }
+    
+    convenience init(digit: String, type: DigitType, index: Int) {
+        self.init(frame: .zero, digit: digit, type: type, index: index)
+    }
+    
+    convenience init(digit: String, type: DigitType) {
+        self.init(frame: .zero, digit: digit, type: type, index: 0)
+    }
+    
+    convenience init(sign: ActivityType) {
+        self.init(frame: .zero, digit: sign.sign, type: .common, index: 0)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func setDigit(_ digit: String) {
+        self.digitLabel.text = "\(digit)"
+        self.digitLabel.textColor = .label
+        borderColor = .systemGray4
+        self.setNeedsDisplay()
+    }
+    
+    func setColor(for answer: Bool) {
+        if answer {
+            digitColor = .green
+            borderColor = .green
+        } else {
+            digitColor = .red
+            borderColor = .red
+        }
+        self.digitLabel.textColor = digitColor
+        self.setNeedsDisplay()
+    }
+    
+    private func setupView() {
+        self.translatesAutoresizingMaskIntoConstraints = false
+        self.backgroundColor = .systemBackground
+        
+        if self.type == .result {
+            self.digitLabel.textColor = digitColor
+        } else {
+            self.digitLabel.textColor = .label
+        }
+        
+        self.addSubview(digitLabel)
+        
+        NSLayoutConstraint.activate([
+            digitLabel.centerYAnchor.constraint(equalTo: self.centerYAnchor),
+            digitLabel.centerXAnchor.constraint(equalTo: self.centerXAnchor)
+        ])
+    }
+    
+    override func draw(_ rect: CGRect) {
+        if self.type == .result {
+            let rectWidth = rect.width - rectBorderWidth
+            let rectHeight = rect.height - rectBorderWidth
+            
+            let xf: CGFloat = (self.frame.width - rectWidth) / 2
+            let yf: CGFloat = (self.frame.height - rectHeight) / 2
+            
+            let rect = CGRect(x: xf, y: yf, width: rectWidth, height: rectHeight)
+            let path = UIBezierPath(roundedRect: rect, cornerRadius: rectCornerRadius)
+            
+            path.lineWidth = rectBorderWidth
+            borderColor.setStroke()
+            path.stroke()
+        }
+    }
+    
+    override func hitTest(_ point: CGPoint, with event: UIEvent?) -> UIView? {
+        guard  self.type == .result, self.bounds.contains(point) else { return nil }
+        return self
+    }
+}
