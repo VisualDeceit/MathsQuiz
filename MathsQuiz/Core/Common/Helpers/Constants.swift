2b5bdae8a6a3cb91cafd64aa336469c7416b0286
Alexander Fomin <inzhener@outlook.com>
2022-01-10 22:19:21 +0300

    CGFloat type

--- MathsQuiz/Core/Common/Helpers/Constants.swift
+++ MathsQuiz/Core/Common/Helpers/Constants.swift
@@ -26,21 +26,21 @@ enum Indent {
 }
 
 enum MQOffset {
-    static let offset4 = 4.0
-    static let offset8 = 8.0
-    static let offset12 = 12.0
-    static let offset16 = 16.0
-    static let offset20 = 20.0
-    static let offset24 = 24.0
-    static let offset28 = 28.0
-    static let offset32 = 32.0
-    static let offset36 = 36.0
-    static let offset44 = 44.0
-    static let offset56 = 56.0
-    static let offset64 = 64.0
-    static let offset68 = 68.0
-    static let offset72 = 72.0
-    static let offset80 = 80.0
-    static let offset120 = 120.0
-    static let offset160 = 160.0
+    static let offset4: CGFloat = 4.0
+    static let offset8: CGFloat = 8.0
+    static let offset12: CGFloat = 12.0
+    static let offset16: CGFloat = 16.0
+    static let offset20: CGFloat = 20.0
+    static let offset24: CGFloat = 24.0
+    static let offset28: CGFloat = 28.0
+    static let offset32: CGFloat = 32.0
+    static let offset36: CGFloat = 36.0
+    static let offset44: CGFloat = 44.0
+    static let offset56: CGFloat = 56.0
+    static let offset64: CGFloat = 64.0
+    static let offset68: CGFloat = 68.0
+    static let offset72: CGFloat = 72.0
+    static let offset80: CGFloat = 80.0
+    static let offset120: CGFloat = 120.0
+    static let offset160: CGFloat = 160.0
 }
