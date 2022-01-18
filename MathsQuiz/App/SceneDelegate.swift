42795a2ff230661e6d69c7aaaf1226690234632a
Tigran Karakhanyan <lev.98@bk.ru>
2022-01-18 15:43:59 +0300

    tranfered snpKit to UserProfileViewController

--- MathsQuiz/App/SceneDelegate.swift
+++ MathsQuiz/App/SceneDelegate.swift
@@ -25,7 +25,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         guard let windowScene = (scene as? UIWindowScene) else { return }
         
         window = UIWindow(frame: UIScreen.main.bounds)
-        window?.rootViewController = PasswordChangeViewController()
+        window?.rootViewController = UserProfileViewController()
         window?.windowScene = windowScene
         window?.makeKeyAndVisible()
 //        appCoordinator = makeCoordinator()
